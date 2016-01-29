7536371e7efb7edece6719ccde447e9c7d44b266
Henrique Moco <hmoco94@vt.edu>
2016-01-28 22:40:34 -0500

    Add template and initial collumn work

--- web.py
+++ web.py
@@ -1,14 +1,24 @@
-""" Heroku/Python Quickstart: https://blog.heroku.com/archives/2011/9/28/python_and_django"""
+""" 
+Heroku/Python Quickstart: 
+https://blog.heroku.com/archives/2011/9/28/python_and_django
+"""
 
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return 'Hello from the SPARK learn-a-thon!'
+    name = 'poo'
+    instagram_pics = [1, 2, 3, 4, 5, 6, 7]  #  instagram_pics()
+    twitter_pics = [1, 2, 3]  #  twitter_pics()
+    return render_template(
+        'home.html', name=name, 
+        instagram_pics=instagram_pics, 
+        twitter_pics=twitter_pics,
+    )
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port, debug=True)
