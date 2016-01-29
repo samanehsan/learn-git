ce35cfb15714869f11e0087eb7c81c4e02d0b498
Saman Ehsan <se8ea@virginia.edu>
2016-01-29 13:07:38 -0500

    Fix settings syntax

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 import os
 
 #Twitter
-CONSUMER_KEY = os.environ("CONSUMER_KEY")
-CONSUMER_SECRET = os.environ("CONSUMER_SECRET")
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 
-ACCESS_KEY = os.environ("ACCESS_KEY")
-ACCESS_SECRET = os.environ("ACCESS_SECRET")
+ACCESS_KEY = os.environ['ACCESS_KEY']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
 #Instagram
-CLIENT_ID = os.environ("CLIENT_ID")
+CLIENT_ID = os.environ['CLIENT_ID']
